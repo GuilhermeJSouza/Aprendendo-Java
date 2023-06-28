b2bbb2c0e0fc1eb7e802d10bc7db8805f1901bb5
GuilhermeJSouza <gjds360@hotmail.com>
2023-06-28 08:58:52 -0300

    Alteração na estrutura das pastas

--- Códigos/Welcome/welcome.java
+++ Códigos/Welcome/welcome.java
@@ -1,7 +1,7 @@
 //Welcome.java
 //Programa de impressão de texto.
 
-public class welcome{
+public class Welcome{
     //método main inicia a execução do aplicativo Java
     public static void main(String[] args)
     {
